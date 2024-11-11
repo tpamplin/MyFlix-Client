99c8558fd910bf81622deef613dda6936bbc8db1
tpamplin <gartiman1@gmail.com>
2024-11-11 17:27:24 -0500

    add links to navbar

--- src/Components/navigation-bar/navigation-bar.jsx
+++ src/Components/navigation-bar/navigation-bar.jsx
@@ -11,7 +11,28 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        
+                        { !user && (
+                            <>
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/signup">
+                                    Signup
+                                </Nav.Link>
+                            </>
+                        )}
+                        { user && (
+                            <>
+                                <Nav.Link as={Link} to="/">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link onClick={onLoggedOut}>
+                                    Logout
+                                </Nav.Link>
+                            </>
+                        )}
+
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
