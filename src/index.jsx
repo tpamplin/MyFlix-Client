a4145ecb45fa9c6f4a48e844203533240973e4aa
tpamplin <gartiman1@gmail.com>
2024-12-18 10:03:03 -0500

    fix netlify error by capitalizing a single letter.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./Components/main-view/main-view";
-import Container from "react-bootstrap/container"
+import Container from "react-bootstrap/Container"
 
 import "./index.scss";
 
